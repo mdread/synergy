db9ea554ce4af53bb869ca183df19dc984035d2d
mdread <maniacal.dread@gmail.com>
2013-10-08 08:54:38 +0200

    SynergyClient recives a ClientConfig object for configuration

--- src/main/scala/net/caoticode/synergy/client/SynergyClient.scala
+++ src/main/scala/net/caoticode/synergy/client/SynergyClient.scala
@@ -13,20 +13,30 @@ import scala.concurrent.ExecutionContext
 import akka.actor.Actor
 import akka.actor.Props
 
-class SynergyClient {
+case class ClientConfig(serverHost: String, serverPort: Int, myHost: String, myPort: Int)
 
-  val config = ConfigFactory.load()
-  val clientConfig = config.getConfig("SynergyClient").withFallback(config)
+class SynergyClient(conf: Option[ClientConfig] = None) {
+
+  val referenceConfig = ConfigFactory.load()
+  val providedConfig = conf.map { config =>
+    ConfigFactory.parseString(s"""
+      akka.remote.netty.tcp.hostname="${config.myHost}"
+      akka.remote.netty.tcp.port=${config.myPort}
+      serverConnection = akka.tcp://SynergyServer@${config.serverHost}:${config.serverPort}/user/channelmaster
+     """)
+  }.getOrElse(ConfigFactory.empty())
+
+  val clientConfig = providedConfig.withFallback(referenceConfig.getConfig("SynergyClient")).withFallback(referenceConfig)
   val system = ActorSystem("SynergyClient", clientConfig)
 
   implicit val timeout = Timeout(5 seconds)
   implicit val ex = system.dispatcher
 
-  val channelMaster = system.actorSelection("akka.tcp://SynergyServer@127.0.0.1:2552/user/channelmaster")
+  val channelMaster = system.actorSelection(clientConfig.getString("serverConnection"))
 
   def createChannel(name: String): ChannelClient = {
     val res = (channelMaster ? ChannelCreate(name)).mapTo[ChannelCreated]
-    
+
     new ChannelClient(res.map(_.channel), system)
   }
 
@@ -35,25 +45,25 @@ class SynergyClient {
   }
 }
 
-class ChannelClient(channelRef: Future[ActorRef], system: ActorSystem){
+class ChannelClient(channelRef: Future[ActorRef], system: ActorSystem) {
   import net.caoticode.synergy.Channel2ClientProtocol._
-  
+
   implicit val ex = system.dispatcher
-  
+
   def publish(message: Any, routingTag: String = ""): Unit = {
-    channelRef.map { channel => channel ! Publish(message, routingTag)}
+    channelRef.map { channel => channel ! Publish(message, routingTag) }
   }
-  
+
   def subscribeForPush[T: Manifest](handler: (T => Unit)): Unit = {
-	  subscribeForPush("", handler)
+    subscribeForPush("")(handler)
   }
-  
-  def subscribeForPush[T: Manifest](routingTag: String, handler: (T => Unit)): Unit = {
-	  channelRef.map { channel => channel.tell(Subscribe(PushSubscription, routingTag), system.actorOf(Props(new PushSubscriberActor[T](handler))))}
+
+  def subscribeForPush[T: Manifest](routingTag: String)(handler: (T => Unit)): Unit = {
+    channelRef.map { channel => channel.tell(Subscribe(PushSubscription, routingTag), system.actorOf(Props(new PushSubscriberActor[T](handler)))) }
   }
-  
+
   // def subscribeForPull() = ???
-  
+
   private class PushSubscriberActor[T](handler: (T => Unit)) extends Actor {
     def receive = {
       case x => handler(x.asInstanceOf[T])
