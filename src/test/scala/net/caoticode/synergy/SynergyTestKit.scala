888c8fe055a93eb7ada6981ccdb0bde8a61c25cd
mdread <maniacal.dread@gmail.com>
2013-10-08 05:49:01 +0200

    tests for channel CREATE/JOIN operations and a basic push-channel

---
+++ src/test/scala/net/caoticode/synergy/SynergyTestKit.scala
@@ -0,0 +1,104 @@
+package net.caoticode.synergy
+
+import akka.actor.{ActorSystem, Actor, ActorLogging, Props}
+import akka.testkit.{TestKit, ImplicitSender, EventFilter}
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
+import org.scalatest.BeforeAndAfterAll
+import com.typesafe.config.ConfigFactory
+import akka.actor.actorRef2Scala
+import net.caoticode.synergy.Channel2ClientProtocol.Publish
+import net.caoticode.synergy.Channel2ClientProtocol.PushSubscription
+import net.caoticode.synergy.Channel2ClientProtocol.Subscribe
+import net.caoticode.synergy.ChannelMasterProtocol.ChannelCreate
+import net.caoticode.synergy.ChannelMasterProtocol.ChannelCreated
+import net.caoticode.synergy.ChannelMasterProtocol.ChannelExists
+import net.caoticode.synergy.ChannelMasterProtocol.ChannelExistsResponse
+import net.caoticode.synergy.ChannelMasterProtocol.ChannelJoin
+import net.caoticode.synergy.ChannelMasterProtocol.ChannelJoinFail
+import net.caoticode.synergy.ChannelMasterProtocol.ChannelJoinSuccess
+import net.caoticode.synergy.server.ChannelMaster
+
+object SynergyTestKit {
+  class LoggerActor extends Actor with ActorLogging{
+    def receive = {
+      case x => 
+        log.info(x.toString)
+        //println(x.toString)
+    }
+  }
+}
+
+class SynergyTestKit(_system: ActorSystem) extends TestKit(_system) with WordSpec
+  with ImplicitSender with MustMatchers with BeforeAndAfterAll {
+
+  def this() = this(ActorSystem("SynergyTestKit", ConfigFactory.parseString("""
+  akka.loggers = ["akka.testkit.TestEventListener"]
+  """)))
+
+  import net.caoticode.synergy.server._
+  import net.caoticode.synergy.ChannelMasterProtocol._
+  import net.caoticode.synergy.Channel2ClientProtocol._
+  import SynergyTestKit._
+  
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+  
+  val chm = system.actorOf(Props[ChannelMaster])
+  
+  "a channel" must {
+	
+    "be created" in {
+      chm ! ChannelCreate("testchannel")
+      
+      expectMsgType[ChannelCreated]
+    }
+    
+    "exist after creation" in {
+      chm ! ChannelExists("testchannel")
+      
+      expectMsg(ChannelExistsResponse(true))
+    }
+    
+    "not exist if has not been created" in {
+      chm ! ChannelExists("idonotexist")
+      
+      expectMsg(ChannelExistsResponse(false))
+    }
+    
+    "be joined" in {
+      chm ! ChannelJoin("testchannel")
+      
+      expectMsgType[ChannelJoinSuccess]
+    }
+    
+    "not be joined if it does not exists" in {
+      chm ! ChannelJoin("idonotexist")
+      
+      expectMsgType[ChannelJoinFail]
+    }
+ 
+  }
+  
+  "the log subscriber" must {
+    
+    "log a message" in {
+      chm ! ChannelJoin("testchannel")
+      
+      val ChannelJoinSuccess(channel) = expectMsgType[ChannelJoinSuccess]
+      
+      val loggerActor = system.actorOf(Props[LoggerActor])
+      
+      channel.tell(Subscribe(PushSubscription), loggerActor)
+      
+      EventFilter.info(message = "ciao", occurrences = 3) intercept {
+    	  channel ! Publish("ciao")
+    	  channel ! Publish("ciao")
+    	  channel ! Publish("ciao")
+      }
+    }
+    
+  }
+  
+}
