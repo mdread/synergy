42f1525e0f88ac639ac1ae9f9f3d24f383145d28
mdread <maniacal.dread@gmail.com>
2013-10-08 07:56:57 +0200

    added some dependencies

--- project/SynergyBuild.scala
+++ project/SynergyBuild.scala
@@ -18,6 +18,7 @@ object SynergyBuild extends Build {
       // dependencies
       libraryDependencies += "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test",
       libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.2.1",
+      libraryDependencies += "com.typesafe.akka" %% "akka-remote" % "2.2.1",
       libraryDependencies += "com.typesafe.akka" %% "akka-testkit" % "2.2.1"
 
     )
